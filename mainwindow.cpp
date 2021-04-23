b73e4223c8521d5cb4e83362215f38df0a212207
motovotcazsvs <motovotcazsvs@gmail.com>
2021-04-23 17:19:31 +0300

    сделан поиск по полю, с конца,

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,28 +16,38 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //QSqlDatabase db;
     db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("D:/QtProject/qmlsqlite/MedicalMeasurements1.db3");//для windows
+    db.setDatabaseName("D:/QtProject/qmlsqlite/MedicalMeasurements.db3");//для windows
     db.open();
 
     // объявляем модель данных
      model = new QSqlTableModel(this);
      // указываем таблицу из БД
-     model->setTable("TABLEMEDICALMEASUREMENTS");
-     // заносим данные в модель
-     // если удачно
-     if(model->select())
-     {
+     model->setTable("TABLEMEDICALMEASUREMENTS"); // Имя таблицы базы данных
+
+
+      model->setFilter(" DATEMONTH >= 1"); // Условие WHERE.
+      model->setSort(0, Qt::DescendingOrder); // Сортировка по убыванию id.
+      model->select(); // Получить данные.
+
+     //if(model->select())
+     //{
          // передаем данные из модели в tableView
+
          ui->tableView->setModel(model);
+         //ui->tableView->hideColumn(0); // don't show the ID
          // устанавливаем высоту строки по тексту
          ui->tableView->resizeRowsToContents();
          // шапка для первой колонки
-         /*model->setHeaderData(0, Qt::Horizontal, tr("ID"));
+         //model->setHeaderData(0, Qt::Horizontal, tr("ID"));
          // шапка для второй колонки
-         model->setHeaderData(1, Qt::Horizontal, tr("имя"));*/
+         //model->setHeaderData(1, Qt::Horizontal, tr("имя"));
          // передача управления элементу tableView
+         ui->tableView->setSortingEnabled(true);
          ui->tableView->setFocus();
-     }
+         //ui->tableView->scrollToBottom();
+
+    // }
+
 
 
 
